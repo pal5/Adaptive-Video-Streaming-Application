5fed4974317e2f084fecf95cd9b03b33247c9153
prabhath <p.prabhath52000@gmail.com>
2019-10-30 08:07:47 +0530

    streaming

--- AVS/app.js
+++ AVS/app.js
@@ -30,7 +30,9 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  /*
   res.status(err.status || 500);
   res.render('error');
+  */
 });
 module.exports = app;
--- AVS/routes/index.js
+++ AVS/routes/index.js
@@ -1,11 +1,48 @@
 var express = require('express');
 var router = express.Router();
-
+var fs=require('fs');
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
 	console.log("hello");
   res.render('index', { title: 'NITK Video Streaming App'} );
+});
+
+router.get('/stuff.mp4',function(req,res)
+{
+   console.log("video requested");
+   dir_name=__dirname;
+   const path = dir_name+'/stuff.mp4';
+   const stat = fs.statSync(path);
+   const fileSize = stat.size;
+   const range = req.headers.range;
+   if (range) {
+     const parts = range.replace(/bytes=/, "").split("-");
+     const start = parseInt(parts[0], 10);
+     const end = parts[1]? parseInt(parts[1], 10): fileSize-1;
+     const chunksize = (end-start)+1;
+     const file = fs.createReadStream(path, {start, end});
+     const head = {
+       'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+       'Accept-Ranges': 'bytes',
+       'Content-Length': chunksize,
+       'Content-Type': 'video/mp4',
+     }
+     res.writeHead(206, head);
+     file.pipe(res);
+   } else {
+     const head = {
+       'Content-Length': fileSize,
+       'Content-Type': 'video/mp4',
+     }
+     res.writeHead(200, head)
+     fs.createReadStream(path).pipe(res);
+   }
+});
+
+router.get('/pictures/pic.png',function(req,res)
+{
+   console.log('picture requested');
 });
 
 router.post('/search', function(req, res){
