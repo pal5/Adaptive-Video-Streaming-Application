d35db11b329936b1bbbc0c8922ad6c4ecf7d3925
prabhath <p.prabhath52000@gmail.com>
2019-11-07 22:28:13 +0530

    added frequency of search

--- AVS/seed/User-seeder.js
+++ AVS/seed/User-seeder.js
@@ -9,8 +9,20 @@ function writeUserData(user_name,search_query)
   var userData = firebase.database().ref("UserData/");
   var userRef=userData.child(user_name);
   var queryRef=userRef.child("query");
-  var queryObj=queryRef.child(search_query);
-  queryObj.set({time:(new Date()).toLocaleString()});
-  
+  queryRef.once('value', function(snapshot) {
+    if(!snapshot.hasChild(search_query))
+    {
+      var queryObj=queryRef.child(search_query);
+      queryObj.set({count:1,time:(new Date()).toLocaleString()})
+    }
+    else
+    {
+      var inc=queryRef.child(search_query);
+      inc.child('count').transaction(function(c) {
+        return c+1;
+      });
+      inc.update({time:(new Date()).toLocaleString()});
+    }
+  });
 }
 module.exports=writeUserData;
